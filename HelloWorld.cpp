9ad4a03f32b99f835cafa941bd113f2c264447c2
srobbins <srobbins@chapman.edu>
2019-09-05 08:33:16 -0700

    Created hello world program

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+//Sean Robbins
+//2328696
+//Sept 5, 2019
+
+#include <iostream>
+
+using namespace std;
+//This program prints Hello World to the console
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
